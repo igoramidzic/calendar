50813019e53edf8c8bd0835bdae65c9e3d802d3e
Igor Amidzic <amidzicigor@yahoo.com>
2017-10-07 11:43:48 -0400

    development: Igor - add a few tests to appComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
@@ -12,9 +13,31 @@ describe('AppComponent', () => {
 		}).compileComponents();
 	}));
 
-	it('should create the app', async(() => {
+	it('should create the app', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
-	}));
+	});
+
+	it('should contain the sidebar component', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('app-sidebar')).toBeTruthy();
+	})
+
+	it('should contain the navbar component', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('app-navbar')).toBeTruthy();
+	})
+
+	it('should contain the router outlet', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('router-outlet')).toBeTruthy();
+	})
+
 });
